6bf54fe42d8e1353ba4c0cba3e7871a6e48ddeb9
KevinLlamas01 <106411747+KevinLlamas01@users.noreply.github.com>
2023-04-25 17:28:22 -0700

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php 
+	
+	define("CLIENT_ID", "AZrEJfmXWTfr3u_oVjnvTgvR_oDZkmtOqzfyfeO9Wx0sejJUaUV8JA0qdYMchxWztGc0FYR9folEp356");
+	define("CURRENCY", "USD");
+	define("KEY_TOKEN", "APR.wqc-354*");
+	define("MONEDA", "$");
+
+	
+
+	$num_cart=0;
+	if(isset($_SESSION['carrito']['producto'])){
+		$num_cart=count($_SESSION['carrito']['producto']);
+	}
+ ?>
